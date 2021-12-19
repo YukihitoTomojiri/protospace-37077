89400db1f25ecd7b0d5cc9f81e2ef6241d3f290e
YukihitoTomojiri <75321573+YukihitoTomojiri@users.noreply.github.com>
2021-12-19 21:22:34 +0900

    prototypeモデルのバリデーション

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,12 @@
 class Prototype < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :consept, presence: true
+  validates :image, presence:true, unless: :was_attached?
+  def was_attached?
+    self.image.attached?
+  end
 end
